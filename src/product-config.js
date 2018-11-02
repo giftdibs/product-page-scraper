05eeb105cd0132c07cd66affbe7674a2bec4601f
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-11-02 09:37:08 -0400

    Added gamestop, target, and walmart

--- src/product-config.js
+++ src/product-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   'amazon.com': {
-    'imageSelectors': [
+    imageSelectors: [
       '#landingImage'
     ],
-    'nameSelectors': [
+    nameSelectors: [
       '#productTitle'
     ],
-    'priceSelectors': [
+    priceSelectors: [
       '#buybox .offer-price',
       '#newPrice .buyingPrice',
       '#priceblock_ourprice',
@@ -15,25 +15,58 @@ module.exports = {
     ]
   },
   'forever21.com': {
-    'imageSelectors': [
+    imageSelectors: [
       'img.product_image'
     ],
-    'nameSelectors': [
+    nameSelectors: [
       '#h1Title'
     ],
-    'priceSelectors': [
+    priceSelectors: [
       '#ItemPrice'
     ]
   },
+  'gamestop.com': {
+    imageSelectors: [
+      '.boxart img'
+    ],
+    nameSelectors: [
+      'h1.ats-prod-title'
+    ],
+    priceSelectors: [
+      'h3.ats-prodBuy-price'
+    ]
+  },
   'kohls.com': {
-    'imageSelectors': [
+    imageSelectors: [
       'img.PDP_heroimage'
     ],
-    'nameSelectors': [
+    nameSelectors: [
       'h1.pdp-product-title'
     ],
-    'priceSelectors': [
+    priceSelectors: [
       '#pdp-Pricing .main-price'
+    ]
+  },
+  'target.com': {
+    imageSelectors: [
+      '[data-test="product-image"] img'
+    ],
+    nameSelectors: [
+      '[data-test="product-title"]'
+    ],
+    priceSelectors: [
+      'span[data-test="product-price"]'
+    ]
+  },
+  'walmart.com': {
+    imageSelectors: [
+      '.product-image-carousel-container img'
+    ],
+    nameSelectors: [
+      'h1.fashion-brand-name'
+    ],
+    priceSelectors: [
+      'span.price-group'
     ]
   }
 };
